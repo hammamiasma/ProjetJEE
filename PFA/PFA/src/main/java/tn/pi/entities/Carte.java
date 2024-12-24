f197b44b5187f9d352392fe2fdf287aeda0db8fc
haasma24 <102852957+haasma24@users.noreply.github.com>
2024-12-24 12:50:26 +0100

    Add files via upload

---
+++ PFA/PFA/src/main/java/tn/pi/entities/Carte.java
@@ -0,0 +1,24 @@
+package tn.pi.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Carte {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String numero;
+    private String type; // Débit ou Crédit
+
+    @ManyToOne
+    @JoinColumn(name = "compte_id")
+    private Compte compte;
+}
